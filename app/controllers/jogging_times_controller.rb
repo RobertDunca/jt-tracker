8c49dd602906253dd8ceae2423fc69ed446d4a1d
RobertDunca <duradi.mail@gmail.com>
2023-04-05 13:03:38 +0300

    Finished filter functionality

--- app/controllers/jogging_times_controller.rb
+++ app/controllers/jogging_times_controller.rb
@@ -1,35 +1,39 @@
 class JoggingTimesController < ApplicationController
-  before_action :authenticate_user!, only: %i[create edit destroy home update]
+  before_action :authenticate_user!, only: %i[create edit destroy index update]
   before_action :correct_user, only: %i[destroy edit update]
 
-  def home
+  def index
     if user_signed_in?
       @jogging_time = current_user.jogging_times.build
       @jogging_times = current_user.my_times
-      if params[:jogging_time].present?
-        start_date = Date.parse(params.require(:jogging_time).permit(:start_date))
-        end_date = Date.parse(params.require(:jogging_time).permit(:end_date))
-        @jogging_times = @jogging_times.where(date: start_date..end_date)
-        redirect_to root_url
+      if params[:from_date].present? || params[:to_date].present?
+        filter_jogging_times
       end
     end
   end
 
   def create
-    @jogging_time = current_user.jogging_times.build(jogging_time_params)
+    parameters = jogging_time_params.except(:from_date, :to_date)
+    @jogging_time = current_user.jogging_times.build(parameters)
     if @jogging_time.save
       redirect_to root_url
+    else
+      @jogging_times = current_user.my_times
+      p @jogging_time.errors.full_messages
+      p @jogging_time.inspect
+      render 'jogging_times/index'
     end
   end
 
   def edit
-    p "#{@jogging_time.id}" + '++++++++++++'
   end
 
   def update
     if @jogging_time.update(jogging_time_params)
       redirect_to root_url
     else
+      p "++++++++++++++"
+      p @jogging_time.errors.full_messages
       render 'edit'
     end
   end
@@ -50,6 +54,17 @@ class JoggingTimesController < ApplicationController
     def correct_user
       @jogging_time = current_user.jogging_times.find_by(id: params[:id])
       redirect_to root_url if @jogging_time.nil?
+    end
+
+    def filter_jogging_times
+      if params[:from_date].present?
+        @from_date = Date.parse(params[:from_date])
+      end
+      if params[:to_date].present?
+        @to_date = Date.parse(params[:to_date])
+      end
+      @jogging_times = @jogging_times
+                      .where(date: (@from_date || Date.new)..(@to_date || Date::Infinity.new))
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jogging_times, only: %i[create destroy edit update]
-  root "jogging_times#home"
+  resources :jogging_times, only: %i[create destroy edit update index]
+  root "jogging_times#index"
 end
